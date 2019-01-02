b0c8ba0cfb3c209863ea216b31a14fa2ab3bcc96
seacow93 <seacow18@gmail.com>
2019-01-02 15:16:34 +0100

    Testing

--- Test.py
+++ Test.py
@@ -1,5 +1,19 @@
 ###########
 ###########This is a Test########
 
-print("TEST")
+user = input("What is your name? :")
 
+print("Welcome", user)
+
+Address = input("please enter your address : ")
+
+Phone_number = input("please enter your phone number : ")
+
+Email_address = input("please enter your email address : ")
+
+print("Info")
+
+print("Name:", user)
+print("Address:", Address)
+print("Phone:", Phone_number)
+print("Email:", Email_address)
